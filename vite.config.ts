01f26f20c01e78c931257223423ba3ec0093753c
Jaka Park <jakapark94@naver.com>
2026-07-09 17:22:34 +0900

    vite.config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/confbasig/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/my-portfolio/",
 });
