7f3cca8eb564ae7eb4c0e209aef200a136a597a8
Jaka Park <jakapark94@naver.com>
2026-07-22 12:40:19 +0900

    feat: Hero text transition 추가

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -1,7 +1,32 @@
 import { ChevronsDown, MouseIcon } from "lucide-react";
 import BackroundImage from "../../assets/hero_section_background.png";
+import { useEffect, useRef, useState } from "react";
 
 const Hero = () => {
+  const textRef = useRef<HTMLHeadingElement | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const target = textRef.current;
+
+    if (!target) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.2 },
+    );
+
+    observer.observe(target);
+
+    return () => {
+      observer.unobserve(target);
+    };
+  }, []);
+
   return (
     <section
       style={{
@@ -10,13 +35,17 @@ const Hero = () => {
       className="relative w-full min-h-dvh flex flex-col justify-center items-center bg-right"
     >
       <div className="absolute z-20">
-        <h2 className="font-extrabold text-center leading-16 text-white/40 ">
+        <h2
+          ref={textRef}
+          className={`font-extrabold text-center leading-16 text-white transition-opacity duration-1000 ${isVisible ? "opacity-40" : "opacity-0"}`}
+        >
           <p className="text-[3.2rem]">'End'가 아닌 'And'</p>
           <p className="text-[3.2rem]">Front'And' 를 꿈꾸는 사람</p>
           <p className="text-[2.8rem] pt-3">잘 부탁드립니다</p>
         </h2>
       </div>
       <div className="absolute bottom-6 flex justify-center z-30 text-white/40 animate-[scrollGuideMove_1s_linear_infinite]">
+        {/* 마우스 스크롤 아이콘 애니메이션 처리 */}
         <div className="flex flex-col justify-center items-center">
           <MouseIcon className="opacity-70" />
           <ChevronsDown className="opacity-70" />
